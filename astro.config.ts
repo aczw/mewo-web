53c427cbaef49292dcde2b0234d163197ec8fe0b
Charles Wang <zwcharl@gmail.com>
2026-02-24 20:51:19 -0500

    robots.txt, astro sitemap integration, block for @font-face

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 /**
  * @see https://astro.build/config
@@ -8,7 +9,10 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   site: "https://mewo.czw.sh",
   trailingSlash: "never",
+
   adapter: vercel({
     imageService: true,
   }),
+
+  integrations: [sitemap()],
 });
