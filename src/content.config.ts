fe166fe99e6e163296e05b52ee949d7a2b715b65
Charles Wang <zwcharl@gmail.com>
2026-04-06 07:31:52 -0400

    blog content collection

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    published: z.coerce.date(),
+  }),
+});
+
+export const collections = { blog };
---
+++ src/content/blog/setup.md
@@ -0,0 +1,8 @@
+---
+title: Setting up C++, Dawn, SDL3, and Dear ImGui in VS Code
+published: 2026-04-06T07:28-04:00
+
+slug: setup
+---
+
+Testing testing. Here's some content.
